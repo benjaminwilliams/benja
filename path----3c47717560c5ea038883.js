5d2b17dfde4ae38f6ff6983c00373f2d1a548dfd
bwilliams <benj@minwilliams.net>
2018-04-21 23:22:17 +1000

    replace old site with comming soon page

---
+++ path----3c47717560c5ea038883.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{103:function(t,a){t.exports={data:{site:{siteMetadata:{title:"benja.minwillams.net"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----3c47717560c5ea038883.js.map
